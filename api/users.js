9fe03f5ac390fe40d635bc6d6b694f5aa536e6e0
sschneeberg <simone.m.schneeberg@gmail.com>
2020-12-19 20:40:26 -0500

    Validate is on save on creation

--- api/users.js
+++ api/users.js
@@ -35,9 +35,7 @@ router.post('/register', (req, res) => {
                     newUser.password = hash;
                     newUser
                         .save()
-                        .then((createdUser) =>
-                            res.status(201).json({ user: createdUser })
-                        )
+                        .then((createdUser) => res.status(201).json({ user: createdUser }))
                         .catch((err) => console.log(err));
                 });
             });
@@ -67,18 +65,13 @@ router.post('/login', (req, res) => {
                         //add any other info you need on the front end here
                     };
                     //sign token and send
-                    jwt.sign(
-                        payload,
-                        JWT_SECRET,
-                        { expiresIn: '1h' },
-                        (error, token) => {
-                            if (error) throw Error;
-                            res.json({
-                                success: true,
-                                token: `Bearer ${token}`
-                            });
-                        }
-                    );
+                    jwt.sign(payload, JWT_SECRET, { expiresIn: 3600000 }, (error, token) => {
+                        if (error) throw Error;
+                        res.json({
+                            success: true,
+                            token: `Bearer ${token}`
+                        });
+                    });
                 } else {
                     res.status(400).json({
                         msg: 'Login information incorrect'
@@ -90,18 +83,14 @@ router.post('/login', (req, res) => {
 });
 
 //GET api/users/current (Private)
-router.get(
-    '/current',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        //passport will check for jwt token for us, if avialable we can access the route
-        res.json({
-            id: req.user._id,
-            username: req.user.username,
-            email: req.user.email
-            //other info if needed
-        });
-    }
-);
+router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
+    //passport will check for jwt token for us, if avialable we can access the route
+    res.json({
+        id: req.user._id,
+        username: req.user.username,
+        email: req.user.email
+        //other info if needed
+    });
+});
 
 module.exports = router;
